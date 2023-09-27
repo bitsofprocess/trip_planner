f1b3a100d8574706f68eb72e95649a000f8af32b
Dinelle Zipser <dinellezipser@gmail.com>
2023-09-27 12:12:23 -0700

    removed NavDrawer

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,34 +1,6 @@
 <template>
 
     <v-app class="rounded rounded-md">
-    <!-- <v-app-bar title="Application bar">
-      <template v-slot:append>
-        <v-btn icon="mdi-logout"></v-btn>
-    
-      </template>
-    </v-app-bar> -->
-
-    <!-- <v-navigation-drawer
-        expand-on-hover
-        rail
-      >
-        <v-list>
-          <v-list-item
-            prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
-            title="Sandra Adams"
-            subtitle="sandra_a88@gmailcom"
-          ></v-list-item>
-        </v-list>
-
-        <v-divider></v-divider>
-
-        <v-list density="compact" nav>
-          <v-list-item prepend-icon="mdi-folder" title="My Trips" value="myfiles"></v-list-item>
-          <v-list-item prepend-icon="mdi-account-multiple" title="Shared with me" value="shared"></v-list-item>
-          <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
-        </v-list>
-      </v-navigation-drawer> -->
-
       <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
         <DestinationView :dests="dests"/>
     </v-main>
