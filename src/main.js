42c25b2a257c580ed67262b6fdf4a769dd249ed6
Dinelle Zipser <dinellezipser@gmail.com>
2023-09-27 12:11:55 -0700

    added  DatePicker

--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@
  * Bootstraps Vuetify and other plugins then mounts the App`
  */
 
+
 // Components
 import App from './App.vue'
-
+import { setupCalendar, Calendar, DatePicker } from 'v-calendar';
+import 'v-calendar/style.css';
 
 // Composables
 import { createApp } from 'vue'
@@ -15,15 +17,17 @@ import { createPinia } from 'pinia'
 // Plugins
 import { registerPlugins } from '@/plugins'
 import '@mdi/font/css/materialdesignicons.css'
-import VueDatePicker from '@vuepic/vue-datepicker';
-import '@vuepic/vue-datepicker/dist/main.css'
 
 const app = createApp(App)
+// app.use(VCalendar, {})
 const pinia = createPinia()
 
 registerPlugins(app)
 
+app.use(setupCalendar, {})
+app.component('VCalendar', Calendar)
+app.component('VDatePicker', DatePicker)
 app.use(pinia)
-app.component('VueDatePicker', VueDatePicker)
+// app.component('VueDatePicker', VueDatePicker)
 app.mount('#app')
 
