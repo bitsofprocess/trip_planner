2eb8dbc62f61f824955f3f5331ba7e913c805596
Dinelle Zipser <dinellezipser@gmail.com>
2023-09-27 12:10:16 -0700

    added DatePicker component

--- src/components/DestinationView.vue
+++ src/components/DestinationView.vue
@@ -1,19 +1,24 @@
 
 <template>
+
   <v-row>
-    <v-col cols="6"><DetailAdder /></v-col> 
-    <!-- <v-col cols="6"><DestinationEditor /></v-col>  -->
-    <v-col cols="6"><DestinationListView :dests="dests"/></v-col> 
+    <v-col cols="6">
+      <v-card class="justify-center">
+        <DatePicker />
+      </v-card>
+      <DetailAdder />
+    </v-col>
+    <v-col cols="6"><DestinationListView :dests="dests"/></v-col>
   </v-row>
 </template>
 
 <script>
+import DatePicker from '../components/DatePicker.vue'
 import DestinationListView from '../components/DestinationListView.vue'
-// import DestinationEditor from '../components/DestinationEditor.vue'
 import DetailAdder from '../components/DetailAdder.vue'
 
 export default {
-  components: {  DestinationListView, DetailAdder },
+  components: {  DestinationListView, DetailAdder, DatePicker },
   props: ['dests']
 }
 </script>
