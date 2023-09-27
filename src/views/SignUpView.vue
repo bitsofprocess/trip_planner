c8cfb77c897cfd49b9f09a5851b016142fa302bb
Dinelle Zipser <dinellezipser@gmail.com>
2023-09-27 12:09:55 -0700

    removed extra app bar

--- src/views/SignUpView.vue
+++ src/views/SignUpView.vue
@@ -1,6 +1,5 @@
 <template>
     <v-app class="rounded rounded-md">
-    <v-app-bar title="Application bar"></v-app-bar>
 
       <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
         <SignUp />
